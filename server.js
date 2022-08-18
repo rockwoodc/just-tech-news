c967fa93513cfdfbef20d658f9ffb6f908b2b2e4
rockwoodc <rockwoodce@gmail.com>
2022-08-18 09:31:34 -0400

    added find all

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(routes);
 
 // turn on connection to db and server
 //The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
